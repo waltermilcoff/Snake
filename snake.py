4329ea0b3ce90439868cd2c52396c5d073fba60e
waltermilcoff <waltermilcoff@gmail.com>
2022-02-17 11:39:18 -0300

    Agregue movimiento a la serpiente (snake)

--- snake.py
+++ snake.py
@@ -1,5 +1,5 @@
-from tracemalloc import stop
 import turtle
+import time
 
 s = turtle.Screen()
 s.setup(650,650)
@@ -14,6 +14,27 @@ snake.penup()
 snake.goto(0,0)
 snake.direction = "stop"
 snake.color("red")
+
+retraso = 0.1
+
+def movimiento():
+    if snake.direction == "up":
+        y = snake.ycor()
+        snake.sety(y + 20)
+    if snake.direction == "down":
+        y = snake.ycor()
+        snake.sety(y - 20)
+    if snake.direction == "right":
+        x = snake.xcor()
+        snake.setx(x + 20)
+    if snake.direction == "left":
+        x = snake.xcor()
+        snake.setx(x - 20)
+
+while True:
+    s.update()
+    movimiento()
+    time.sleep(retraso)
 
 
 turtle.done()
